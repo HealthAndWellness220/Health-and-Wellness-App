2e28e9cc0478d959dbbb30a7c1bb9600e7393f1d
HealthAndWellness220 <38298451+HealthAndWellness220@users.noreply.github.com>
2018-04-17 15:37:30 -0500

    Add files via upload

---
+++ Maryville Health and Wellness/Maryville Health and Wellness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maryville Health and Wellness
+//
+//  Created by Alex Hunt on 4/10/18.
+//  Copyright © 2018 Alex Hunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
